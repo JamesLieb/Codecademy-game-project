7a188d647c9988ab240581f6c8095a84e1fb23f7
James <james_lieb@hotmail.com>
2021-06-26 03:15:19 -0400

    This is the end

--- Game.py
+++ Game.py
@@ -1,28 +1,15 @@
 
+print("Hello, please insert your principal mortgage amount, your interest rate on the mortgage, followed by the total number of payments in the following format 1,2,3")
+a=input()
+l=a.split(',')
+P=float(l[0])
+r=float(l[1])
+N=float(l[2])
+r=(r/100)/12
+monthly_payment=round((r*P)/(1-(1+r)**(-N)),2)
+total_interest=round(monthly_payment*N-P,2)
+print("Your expected monthly payment is ${monthly} and your expected interest paid is ${interest}.".format(monthly=monthly_payment,interest=total_interest))
 
 
-
-
-
-
-class Calculator():
-    def __init__(self,P,r,N):
-        self.P=P
-        self.r=r
-        self.N=N
-    def calculate(self):
-        self.r=(self.r/100)/12
-        try:
-            monthly_payment=((self.r)*(self.P))/(1-(1+self.r)**(-self.N))
-            return round(monthly_payment,2)
-        except:
-            ValueError
-            print("Only use numbers for the input, and make sure to seperate the values by a comma")
-            return
-
-        
-    
-example=Calculator(200000 ,6.5,360)
-print(example.calculate())
     
     
