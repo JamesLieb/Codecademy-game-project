cef1df7b729372a8cae841a9e74a65c16fabe989
James <james_lieb@hotmail.com>
2021-06-26 02:49:42 -0400

    Finished the project without an input function

--- Game.py
+++ Game.py
@@ -1,28 +1,28 @@
-class User:
-    user=input()
-    def __repr__(self):
-        print("Hello, please insert the principal amount of your loan, the interest rate and the total number of monthly payments, in the following format (1,2,3)")
 
 
 
 
 
 
-class Calculator(User):
-    def __init__(self):
-        self.P=user[0]
-        self.r=user[1]
-        self.N=[user[2]]
+
+class Calculator():
+    def __init__(self,P,r,N):
+        self.P=P
+        self.r=r
+        self.N=N
     def calculate(self):
+        self.r=(self.r/100)/12
         try:
             monthly_payment=((self.r)*(self.P))/(1-(1+self.r)**(-self.N))
+            return round(monthly_payment,2)
         except:
             ValueError
             print("Only use numbers for the input, and make sure to seperate the values by a comma")
+            return
 
-        return round(monthly_payment,2)
+        
     
-example=Calculator()
-print(example.calculate((200000,6,300)))
+example=Calculator(200000 ,6.5,360)
+print(example.calculate())
     
     
